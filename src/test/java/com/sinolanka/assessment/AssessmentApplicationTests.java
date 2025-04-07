7aef1a8077157c5e0544b8ac3afa047d1100d7f2
FayasAhmed-98 <teamaliens.b18it@gmail.com>
2025-04-07 12:27:14 +0530

    Initial commit

---
+++ src/test/java/com/sinolanka/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinolanka.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
